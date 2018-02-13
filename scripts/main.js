1adb685eed6b88fe062e1f4b056b4a0145df060f
yan1906 <yansokolovsky2000@gmail.com>
2018-02-13 15:11:13 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -22,9 +22,9 @@ var myImage = document.querySelector('img');
 myImage.onclick = function() {
 	var mySrc = myImage.getAttribute('src');
 	if(mySrc === 'images/firefox-icon.png') {
-      myImage.setAttribute ('src','images/firefox2.png');
+      		myImage.setAttribute ('src','images/safari-icon.png');
 	} else {
-	  myImage.setAttribute ('src','images/firefox-icon.png');
+	  	myImage.setAttribute ('src','images/firefox-icon.png');
 	}
 }
 
